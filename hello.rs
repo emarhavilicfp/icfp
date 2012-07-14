83fb1fa04fdefa54ce796f1f6fd51f8631b61107
Josiah Boning <jboning@gmail.com>
2012-07-14 00:32:37 -0400

    Removed hello.rs

--- hello.rs
+++
@@ -1,8 +0,0 @@
-use std;
-import std::list;
-
-fn main() {
-	let list = list::from_vec(["!", "world", " ", "ello", "H"]/_);
-	let s = list::foldl("", list, |accum,elem| elem+accum);
-	io::println(s);
-}
