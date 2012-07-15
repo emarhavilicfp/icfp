dc8e3940ba81138acea6615838506d66973a54d5
Ben Blum <bblum@andrew.cmu.edu>
2012-07-14 20:54:37 -0400

    Implement search.

--- play.rs
+++ play.rs
@@ -69,11 +69,14 @@ fn path_aggressive(_s: state::state, _fire: @mut option<brushfire>)
  * Code
  ****************************************************************************/
 
+type path_state = (@mut option<brushfire>, @mut option<brushfire>);
+fn initial_path_state() -> path_state { (@mut none, @mut none) }
+
 // Finds a path to the lambda that makes us happiest.
-fn get_next_lambda(s: state::state) -> option<(state::state,path::path)> {
-    let easy_state = @mut none;
+fn get_next_lambda(s: state::state, ps: path_state)
+        -> option<(state::state,path::path)> {
+    let (easy_state,aggr_state) = ps;
     let mut easy = path_easy(s, easy_state);
-    let aggr_state = @mut none;
     let mut aggressive = path_aggressive(s, aggr_state);
 
     // Diamonds are forever.
@@ -109,39 +112,99 @@ fn get_next_lambda(s: state::state) -> option<(state::state,path::path)> {
         }
     }
 }
+fn get_next_lambda_oneshot(s: state::state)
+        -> option<(state::state,path::path)> {
+    get_next_lambda(s, initial_path_state())
+}
 
-// Repeatedly finds lambdas (hopefully). Generates the movelist backwards.
-fn greedy_finish(-s: state::state, verbose: bool)
-        -> (dvec::dvec<state::move>, state::state) {
-    // Attempt to do something next.
-    let result = get_next_lambda(s);
-    if result.is_some() {
-        let (newstate,path) = option::unwrap(result);
-        // Find what to do next.
-        if verbose {
-            io::println("Pursuing path of " + str::concat(vec::map(path, |i| { i.to_str() })));
-        }
+// Movelist in reverse order. End state. Best score.
+type search_result = (dvec::dvec<state::move>, state::state, uint);
 
-        let (finishing_moves,endstate) = greedy_finish(newstate, verbose);
-        // Do this stuff before.
-        for path_for_each(path) |the_move| {
-            finishing_moves.push(the_move);
-        }
-        (finishing_moves, endstate)
-    } else {
-        // All done.
-        (dvec::from_elem(state::A), s)
+fn add_path_prefix(finishing_moves: dvec::dvec<state::move>, p: path::path) {
+    for path_for_each(p) |the_move| {
+        finishing_moves.push(the_move);
     }
 }
 
-// type search_opts = 
+type search_opts = { branch_factor: uint, verbose: bool };
+fn default_opts() -> search_opts {
+    { branch_factor: 1, verbose: false }
+}
+fn default_opts_verbose(verbose: bool) -> search_opts {
+    { branch_factor: 1, verbose: verbose }
+}
+fn default_opts_bfac(bf: uint) -> search_opts {
+    { branch_factor: bf, verbose: false }
+}
 
-//fn search(s: state::state, -moves_so_far: dvec::dvec<state::move>,
-//          o: search_opts) -> (
+// Repeatedly finds lambdas (hopefully).
+fn greedy_finish(-s: state::state, o: search_opts) -> search_result {
+    // Attempt to do something next.
+    let result = get_next_lambda_oneshot(s);
+    if result.is_some() {
+        let (newstate,path) = option::unwrap(result);
+        if o.verbose {
+            io::println("Pursuing path of " +
+                        str::concat(vec::map(path, |i| { i.to_str() })));
+        }
+        // Find what to do next.
+        let (finishing_moves,endstate,score) = greedy_finish(newstate, o);
+        // Do this stuff before.
+        add_path_prefix(finishing_moves, path);
+        (finishing_moves, endstate, score)
+    } else {
+        // All done.
+        (dvec::from_elem(state::A), s, 0) // TODO add score
+    }
+}
+
+fn search(-s: state::state, depth: uint, o: search_opts) -> search_result {
+    let mut best = none;
+    let mut best_score = none; // Redundant. To avoid unwrapping 'best'.
+    let path_state = initial_path_state();
+    // Test for horizon node.
+    if depth == 0 {
+        ret greedy_finish(s, o);
+    } else {
+        // Iterate over possibilities.
+        // TODO: maybe prune later?
+        for iter::repeat(o.branch_factor) {
+            let target_opt = get_next_lambda(s, path_state);
+            if target_opt.is_some() {
+                let (newstate,path) = option::unwrap(target_opt);
+                // Recurse.
+                let (finishing_moves,endstate,this_score) =
+                    search(newstate, depth-1, o);
+                // Update best.
+                if best_score.is_none() || this_score > best_score.get() {
+                    // Prepend the moves we had (don't bother if not best)
+                    // TODO: this can probably be tweaked even more to only
+                    // append once per search node.
+                    add_path_prefix(finishing_moves, path);
+                    best = some((finishing_moves, endstate, this_score));
+                    best_score = some(this_score);
+                } else {
+                    again;
+                }
+            } else {
+                // No targets found, huh?
+                break;
+            }
+        }
+        // Process best_moves and best_score.
+        if best.is_some() {
+            option::unwrap(best)
+        } else {
+            // Terminal state. Nothing to do.
+            (dvec::from_elem(state::A), s, 0) // TODO add score
+        }
+    }
+}
 
 fn play_game(+s: state::state, verbose: bool)
-    -> (~[mut state::move], state::state) {
-    let (moves_rev, endstate) = greedy_finish(s, verbose);
+        -> (~[mut state::move], state::state) {
+    let (moves_rev, endstate, _score) =
+        greedy_finish(s, default_opts_verbose(verbose));
     let moves = dvec::unwrap(moves_rev);
     vec::reverse(moves);
     (moves,endstate)
@@ -149,15 +212,59 @@ fn play_game(+s: state::state, verbose: bool)
 
 mod test {
     #[test]
-    fn play_game_check_hash() {
+    fn test_play_game_check_hash() {
         let s = #include_str("./maps/contest10.map");
         let mut s = state::read_board(io::str_reader(s));
-        let mut result = get_next_lambda(s);
+        let mut result = get_next_lambda_oneshot(s);
         while result != none {
             let (newstate, _path) = option::unwrap(result);
             assert newstate.hash() == newstate.rehash();
             s = newstate;
-            result = get_next_lambda(s);
+            result = get_next_lambda_oneshot(s);
         }
+    }
+    #[test]
+    fn test_zero_depth_equals_greedy() {
+        let s = #include_str("./maps/contest10.map");
+        let mut s = state::read_board(io::str_reader(s));
+        let (_, endstate, score) = greedy_finish(copy s, default_opts());
+        let (_, endstate2, score2) = search(copy s, 0, default_opts_bfac(0));
+        let (_, endstate3, score3) = search(s, 0, default_opts_bfac(31337));
+        assert endstate.grid.hash == endstate2.grid.hash;
+        assert endstate.grid.hash == endstate3.grid.hash;
+        assert score == score2;
+        assert score == score3;
+    }
+    #[test]
+    fn test_one_bf_equals_greedy() {
+        let s = #include_str("./maps/contest10.map");
+        let mut s = state::read_board(io::str_reader(s));
+        let (_, endstate, score) = greedy_finish(copy s, default_opts());
+        let (_, endstate2, score2) = search(copy s, 1, default_opts_bfac(1));
+        let (_, endstate3, score3) = search(copy s, 10, default_opts_bfac(1));
+        let (_, endstate4, score4) = search(s, 31337, default_opts_bfac(1));
+        assert endstate.grid.hash == endstate2.grid.hash;
+        assert endstate.grid.hash == endstate3.grid.hash;
+        assert endstate.grid.hash == endstate4.grid.hash;
+        assert score == score2;
+        assert score == score3;
+        assert score == score4;
+    }
+    #[cfg(test)]
+    fn test_search_beats_greedy_help(depth: uint, bf: uint) {
+        let s = #include_str("./maps/contest10.map");
+        let mut s = state::read_board(io::str_reader(s));
+        let (_, endstate, score) = greedy_finish(copy s, default_opts());
+        let (_, endstate2, score2) = search(s, depth, default_opts_bfac(bf));
+        assert score2 > score;
+        #error["Search @ depth %u bfac %u beat greedy %u-%u",
+               depth, bf, score2, score];
+    }
+    #[test]
+    #[ignore]
+    fn test_search_beats_greedy() {
+        test_search_beats_greedy_help(1, 2);
+        test_search_beats_greedy_help(2, 2);
+        test_search_beats_greedy_help(2, 3);
     }
 }
