54b48c099604ee901658c18c66457ef73a30fef0
Ben Blum <bblum@andrew.cmu.edu>
2012-07-15 18:49:46 -0400

    simple evaluation - only evaluates 'safe' lambdas in the bush

--- evaluate.rs
+++ evaluate.rs
@@ -1,11 +1,17 @@
 // A lambda calculus evaluation heuristics.
 
 import state;
+import state::extensions;
 import dlist;
+import dlist::extensions;
 import dvec::extensions;
+import heuristics;
+import bblum = path_find::brushfire;
+import path;
+import path::target;
 
 // List of pairs (position, score)
-type lambdalist = dvec::dvec<(state::coord, int)>;
+type lambdalist = dlist::dlist<(state::coord, int)>;
 
 fn add_lambda_sorted(ls: lambdalist, coord: state::coord, score: int) {
     let mut link = ls.peek_n();
@@ -33,6 +39,37 @@ fn score_lambdas(g: state::grid, f: fn(state::coord) -> int) -> lambdalist {
     }
 }
 
+// day 3 evaluator
+
+const lambda_in_bush_score: int =
+    state::lambda_score / heuristics::bird_in_hand_multiplier;
+
+fn evaluate(s: state::state) -> int {
+    let mut score = s.score;
+    // FIXME: open-coded special-case pathfinder. joshua mad.
+    let targets = bblum::map_mut(s.grid.lambdas(), |x| some(x));
+    let easy_state = @mut none;
+    let mut easy_target = path_easy(s, easy_state, targets);
+    // Don't attempt to validate.
+    while easy_target.is_some() {
+        let (path,sq) = easy_target.get();
+        alt s.grid.at(sq) {
+            // TODO: Horocks and razors go here!
+            state::lambda {
+                score += lambda_in_bush_score;
+            }
+            state::lift_o {
+                // Won game, it seems. The rest of the search should find this
+                // so no need to incorporate it in the eval.
+            }
+            _ {}
+        }
+        easy_target = path_easy(s, easy_state, targets);
+    }
+    score
+}
+
+/* // Kemp Urfy
 fn evaluate(state: state, expensive: bool) -> int {
     let const drown = 4;
     let const trap = 2;
@@ -64,3 +101,47 @@ fn evaluate(state: state, expensive: bool) -> int {
         }
     }
 }
+*/
+
+// Heinous glue.
+// NB: Unlike the one in brushfire.rs, this one returns a coord, not a path.
+fn path_easy(s: state::state, fire: @mut option<bblum::brushfire>,
+             targets: &[mut option<state::coord>])
+        -> option<(path::path,state::coord)> {
+    // Sets a slot in the targets list to 'none'
+    fn process_pathres(-x: option<(path::path,state::coord)>,
+                       targets: &[mut option<state::coord>])
+            -> option<(path::path,state::coord)> {
+        if x.is_some() {
+            let (path,target_found) = option::unwrap(x);
+            // XXX XXX XXX: Asymptotic runtime loss: Fix genpaths to return
+            // an *index*, not a coord.
+            let mut i = 0;
+            while i < targets.len() {
+                if targets[i] == some(target_found) {
+                    targets[i] = none;
+                    break;
+                }   
+                i += 1;
+            }   
+            some((path,target_found))
+        } else { none }
+    }   
+    // TODO: write this to produce a better datatype to begin with
+    if fire.is_some() {
+        let mut shit = none;
+        *fire <-> shit;
+        let (shit1, shit2) = option::unwrap(shit);
+        // Get path and new state
+        let (pathres, stateres) =
+            path::genpath_restart(s.grid, s.robotpos, targets, shit1, shit2);
+        *fire = some(stateres);
+        process_pathres(pathres, targets)
+    } else {
+        let (pathres, stateres) = path::genpaths(s.grid, s.robotpos, targets);
+        *fire = some(stateres);
+        process_pathres(pathres, targets)
+    }   
+
+}
+
