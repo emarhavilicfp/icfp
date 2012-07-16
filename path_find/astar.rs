bf4f58fd90597b3de166c4c30c2b0e2f078ff797
Kevin Murphy <kemurphy@andrew.cmu.edu>
2012-07-16 06:26:42 -0400

    Patterns

--- path_find/astar.rs
+++ path_find/astar.rs
@@ -14,7 +14,19 @@ import path::target;
 import dlist;
 import dlist::extensions;
 
-enum t {_dummy}
+fn mk_hasher(s: state::state) -> (fn@(state::coord) -> uint) {
+    let len = s.grid.grid.len();
+    fn @(c:state::coord) -> uint {
+        let (x, y) = c;
+        (y * len) + x
+    }
+}
+
+fn eqcoord(a: state::coord, b: state::coord) -> bool {
+    a == b
+}
+
+type t = {pats: ~[@pattern::pat]};
 impl of path_find for t {
     fn get_paths(s: state::state) ->
             (fn @() -> option<(state::state, path::path)>) {
@@ -24,6 +36,8 @@ impl of path_find for t {
             pqins(targets, target, state::taxicab_distance(s.robotpos, target));
         }
         //let targets = dlist::from_vec(s.grid.lambdas().map(|x| (1, x));
+        let patterns = pattern::matched_pats(s.grid, self.pats);
+        let patmap = map::hash_from_vec(mk_hasher(s), eqcoord, patterns);
 
         fn @() -> option<(state::state, path::path)> {
             loop {
@@ -32,7 +46,7 @@ impl of path_find for t {
                 }
 
                 let (_, (x, y)) = option::unwrap(targets.pop());
-                let result = navigar(s, (x, y));
+                let result = navigar(s, (x, y), patmap);
                 if (result.is_none()) {
                     again;
                 }
@@ -44,7 +58,7 @@ impl of path_find for t {
 }
 
 fn mk() -> path_find {
-    let t = _dummy;
+    let t = {pats: pattern::read_patterns("patterns/some_patterns")};
     t as path_find
 }
 
@@ -56,7 +70,7 @@ pure fn map_mut<T, U>(v: &[T], f: fn(T) -> U) -> ~[mut U] {
     ret result;
 }
 
-type astarhelp = (state::coord, state::move, path::path, uint);
+type astarhelp = (state::coord, option<state::move>, path::path, uint);
 type pq<T> = dlist::dlist<(uint, T)>;
 
 fn pqins<T: copy>(pq: pq<T>, troll: T, prio: uint) {
@@ -73,17 +87,8 @@ fn pqins<T: copy>(pq: pq<T>, troll: T, prio: uint) {
     pq.push((prio, troll));
 }
 
-fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::path)> {
-    fn mk_hasher(s: state::state) -> (fn@(state::coord) -> uint) {
-        let len = s.grid.grid.len();
-        fn @(c:state::coord) -> uint {
-            let (x, y) = c;
-            (y * len) + x
-        }
-    };
-    fn eqcoord(a: state::coord, b: state::coord) -> bool {
-        a == b
-    }
+fn navigar(s: state::state, dest: state::coord, patmap: map::hashmap<state::coord, @pattern::pat>)
+           -> option<(state::state, path::path)> {
     fn mk_cost(s: state::state) -> (fn@(state::coord, state::coord, state::move) -> uint) {
         let h = s.grid.grid.len();
         fn @(a:state::coord, b:state::coord, m:state::move, copy s) -> uint {
@@ -91,9 +96,9 @@ fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::p
             /* TODO lift this somewhere and better heuristics */
             alt s.grid.at(b) {
                 state::earth |
-                state::empty { c = 10 }
-                state::razor { c = 8 }
-                state::lambda { c = 6 }
+                state::empty { c = 5 }
+                state::razor { c = 4 }
+                state::lambda { c = 3 }
                 state::lift_o { ret 1; }
                 _ { ret 0; }
             }
@@ -144,7 +149,7 @@ fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::p
     let mut visited = map::hashmap(mk_hasher(s), eqcoord);
     let mut pq = dlist::create::<(uint, astarhelp)>();
     let cost = mk_cost(s);
-    pqins(pq, (s.robotpos, state::W, ~[], 0), 1);
+    pqins(pq, (s.robotpos, none, ~[], 0), 1);
 
     while (!pq.is_empty()) {
         let mut (_, (spot, m, path, oldcost)) = option::unwrap(pq.pop());
@@ -152,7 +157,10 @@ fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::p
             again;
         }
 
-        vec::push(path, m);
+        if (m.is_some()) {
+            vec::push(path, option::unwrap(m));
+        }
+
         if (spot == dest) {
             let mut newstate = state_apply(s, path);
             if (newstate.is_some()) {
@@ -166,8 +174,6 @@ fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::p
             }
             again;
         }
-
-        map::set_add(visited, spot);
 
         let spot_;
         alt s.grid.at(spot) {
@@ -185,18 +191,37 @@ fn navigar(s: state::state, dest: state::coord) -> option<(state::state, path::p
             _ { spot_ = spot }
         }
 
+        map::set_add(visited, spot_);
+
+
         for s.grid.neighbors_of(spot_) |n, m_| {
             if (visited.contains_key(n)) {
                 again;
             }
 
-            let tempcost = cost(spot_, n, m_);
-            if (tempcost == 0) {
-                again;
-            }
+            alt patmap.find(n) {
+                some(pat) {
+                    let mut pathnext = ~[];
+                    let mut accum = path;
+                    vec::push(pathnext, m_);
+                    for pat.cmd.each |cmd| {
+                        vec::push(pathnext, copy cmd);
+                    }
+                    for pathnext.each |cmd| {
+                        pqins(pq, (n, some(cmd), accum, oldcost), oldcost);
+                        vec::push(accum, cmd);
+                    }
+                }
+                none {
+                    let tempcost = cost(spot_, n, m_);
+                    if (tempcost == 0) {
+                        again;
+                    }
 
-            let newcost = oldcost + tempcost;
-            pqins(pq, (n, m_, path, newcost), newcost + distance(dest, n));
+                    let newcost = oldcost + tempcost;
+                    pqins(pq, (n, some(m_), path, newcost), newcost);
+                }
+            }
         }
     }
     ret none;
